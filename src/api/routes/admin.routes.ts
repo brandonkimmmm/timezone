d47df8ef86a32208690e2f2de97f67cb5f71e39e
Brandon Kim <bkim2490@gmail.com>
2021-12-10 14:54:48 +0900

    routes refactor

--- src/api/routes/admin.routes.ts
+++ src/api/routes/admin.routes.ts
@@ -4,14 +4,15 @@ import * as authenticate from '../middleware/authenticate';
 import * as validator from '../middleware/validator';
 const router = express.Router();
 
-router.get('/admin/user/timezones', [ authenticate.validateJwtToken, validator.getAdminTimezones ], adminControllers.getTimezones);
-router.post('/admin/user/timezone', [ authenticate.validateJwtToken, validator.postAdminTimezone ], adminControllers.postTimezone);
-router.put('/admin/user/timezone', [ authenticate.validateJwtToken, validator.putAdminTimezone ], adminControllers.putTimezone);
-router.delete('/admin/user/timezone', [ authenticate.validateJwtToken, validator.deleteAdminTimezone ], adminControllers.deleteTimezone);
-router.put('/admin/user/role', [ authenticate.validateJwtToken, validator.putAdminUserRole ], adminControllers.putUserRole);
-router.get('/admin/user', [ authenticate.validateJwtToken, validator.getAdminUser ], adminControllers.getUser);
-router.delete('/admin/user', [ authenticate.validateJwtToken, validator.deleteAdminUser ], adminControllers.deleteUser);
-router.get('/admin/users', [ authenticate.validateJwtToken, validator.getAdminUsers ], adminControllers.getUsers);
-router.post('/admin/user', [ authenticate.validateJwtToken, validator.postSignup ], adminControllers.createUser);
+router.use(authenticate.validateJwtToken);
+router.get('/admin/user/timezones', validator.getAdminTimezones, adminControllers.getTimezones);
+router.post('/admin/user/timezone', validator.postAdminTimezone, adminControllers.postTimezone);
+router.put('/admin/user/timezone', validator.putAdminTimezone, adminControllers.putTimezone);
+router.delete('/admin/user/timezone', validator.deleteAdminTimezone, adminControllers.deleteTimezone);
+router.put('/admin/user/role', validator.putAdminUserRole, adminControllers.putUserRole);
+router.get('/admin/user', validator.getAdminUser, adminControllers.getUser);
+router.delete('/admin/user', validator.deleteAdminUser, adminControllers.deleteUser);
+router.get('/admin/users', validator.getAdminUsers, adminControllers.getUsers);
+router.post('/admin/user', validator.postSignup, adminControllers.createUser);
 
 export default router;
--- src/api/routes/user.routes.ts
+++ src/api/routes/user.routes.ts
@@ -4,10 +4,11 @@ import * as authenticate from '../middleware/authenticate';
 import * as validator from '../middleware/validator';
 const router = express.Router();
 
-router.get('/user', authenticate.validateJwtToken, userControllers.get);
-router.get('/user/timezones', authenticate.validateJwtToken, userControllers.getTimezones);
-router.post('/user/timezone', [ authenticate.validateJwtToken, validator.postUserTimezone ], userControllers.postTimezone);
-router.put('/user/timezone', [ authenticate.validateJwtToken, validator.putUserTimezone ], userControllers.putTimezone);
-router.delete('/user/timezone', [ authenticate.validateJwtToken, validator.deleteUserTimezone ], userControllers.deleteTimezone);
+router.use(authenticate.validateJwtToken);
+router.get('/user', userControllers.get);
+router.get('/user/timezones', userControllers.getTimezones);
+router.post('/user/timezone', validator.postUserTimezone, userControllers.postTimezone);
+router.put('/user/timezone', validator.putUserTimezone, userControllers.putTimezone);
+router.delete('/user/timezone', validator.deleteUserTimezone, userControllers.deleteTimezone);
 
 export default router;
