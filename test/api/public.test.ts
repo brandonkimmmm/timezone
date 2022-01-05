aca1fe7779252718bc55cb629e75414da497a2c1
Brandon Kim <bkim2490@gmail.com>
2022-01-05 15:20:33 +0900

    use supertest

--- test/api/public.test.ts
+++ test/api/public.test.ts
@@ -1,11 +1,9 @@
-import chai, { expect } from 'chai';
-import chaiHttp from 'chai-http';
+import request from 'supertest';
+import { expect } from 'chai';
 import app from '../../src/app';
 import { truncate } from '../utils/db';
 import { name, version } from '../../package.json';
 import { getMockUser } from '../utils/mock';
-
-chai.use(chaiHttp);
 
 const USER = getMockUser();
 
@@ -20,11 +18,11 @@ describe('Public endpoints', () => {
 
 	describe('GET /health', () => {
 		it('it should return app version and name', (done) => {
-			chai.request(app)
+			request(app)
 				.get('/health')
+				.expect(200)
 				.end((err, res) => {
 					expect(err).to.not.exist;
-					expect(res).to.have.status(200);
 					expect(res.body).to.eql({ name, version });
 					done();
 				});
@@ -33,16 +31,16 @@ describe('Public endpoints', () => {
 
 	describe('POST /signup', () => {
 		it('it should return new user email, role, and id', (done) => {
-			chai.request(app)
+			request(app)
 				.post('/signup')
 				.send({
 					email: USER.email,
 					password: USER.password,
 					password_confirmation: USER.password
 				})
+				.expect(201)
 				.end((err, res) => {
 					expect(err).to.not.exist;
-					expect(res).to.have.status(201);
 					expect(res.body)
 						.to.have.all.keys(
 							'id',
@@ -61,16 +59,16 @@ describe('Public endpoints', () => {
 		});
 
 		it('it should return 400 if email is invalid', (done) => {
-			chai.request(app)
+			request(app)
 				.post('/signup')
 				.send({
 					email: 'nope',
 					password: USER.password,
 					password_confirmation: USER.password
 				})
+				.expect(400)
 				.end((err, res) => {
 					expect(err).to.not.exist;
-					expect(res).to.have.status(400);
 					expect(res.body).to.eql({
 						message: '"email" must be a valid email'
 					});
@@ -79,16 +77,16 @@ describe('Public endpoints', () => {
 		});
 
 		it('it should return 400 if password is invalid', (done) => {
-			chai.request(app)
+			request(app)
 				.post('/signup')
 				.send({
 					email: USER.email,
 					password: 'hi',
 					password_confirmation: 'hi'
 				})
+				.expect(400)
 				.end((err, res) => {
 					expect(err).to.not.exist;
-					expect(res).to.have.status(400);
 					expect(res.body).to.eql({
 						message:
 							'"password" with value "hi" fails to match the required pattern: /^[a-zA-Z0-9]{8,20}$/'
@@ -98,16 +96,16 @@ describe('Public endpoints', () => {
 		});
 
 		it('it should return 400 if password_confirmation does not match password', (done) => {
-			chai.request(app)
+			request(app)
 				.post('/signup')
 				.send({
 					email: USER.email,
 					password: USER.password,
 					password_confirmation: 'nope1avasdf23'
 				})
+				.expect(400)
 				.end((err, res) => {
 					expect(err).to.not.exist;
-					expect(res).to.have.status(400);
 					expect(res.body).to.eql({
 						message:
 							'"password_confirmation" must be [ref:password]'
@@ -117,16 +115,16 @@ describe('Public endpoints', () => {
 		});
 
 		it('it should return 400 error if user already exists', (done) => {
-			chai.request(app)
+			request(app)
 				.post('/signup')
 				.send({
 					email: USER.email,
 					password: USER.password,
 					password_confirmation: USER.password
 				})
+				.expect(400)
 				.end((err, res) => {
 					expect(err).to.not.exist;
-					expect(res).to.have.status(400);
 					expect(res.body).to.eql({
 						message: `User ${USER.email} already exists`
 					});
@@ -137,15 +135,15 @@ describe('Public endpoints', () => {
 
 	describe('POST /login', () => {
 		it('it should return bearer token on successful login', (done) => {
-			chai.request(app)
+			request(app)
 				.post('/login')
 				.send({
 					email: USER.email,
 					password: USER.password
 				})
+				.expect(200)
 				.end((err, res) => {
 					expect(err).to.not.exist;
-					expect(res).to.have.status(200);
 					expect(res.body).to.have.all.keys('token');
 					expect(res.body.token).to.be.a('string').to.not.be.empty;
 					done();
@@ -153,15 +151,15 @@ describe('Public endpoints', () => {
 		});
 
 		it('it should return 400 error if email is invalid', (done) => {
-			chai.request(app)
+			request(app)
 				.post('/login')
 				.send({
 					email: 'nope',
 					password: USER.password
 				})
+				.expect(400)
 				.end((err, res) => {
 					expect(err).to.not.exist;
-					expect(res).to.have.status(400);
 					expect(res.body).to.eql({
 						message: '"email" must be a valid email'
 					});
@@ -170,15 +168,15 @@ describe('Public endpoints', () => {
 		});
 
 		it('it should return 400 error if password is invalid', (done) => {
-			chai.request(app)
+			request(app)
 				.post('/login')
 				.send({
 					email: USER.email,
 					password: 'hi'
 				})
+				.expect(400)
 				.end((err, res) => {
 					expect(err).to.not.exist;
-					expect(res).to.have.status(400);
 					expect(res.body).to.eql({
 						message:
 							'"password" with value "hi" fails to match the required pattern: /^[a-zA-Z0-9]{8,20}$/'
@@ -188,15 +186,15 @@ describe('Public endpoints', () => {
 		});
 
 		it('it should return 400 error if user is not found', (done) => {
-			chai.request(app)
+			request(app)
 				.post('/login')
 				.send({
 					email: getMockUser().email,
 					password: USER.password
 				})
+				.expect(400)
 				.end((err, res) => {
 					expect(err).to.not.exist;
-					expect(res).to.have.status(400);
 					expect(res.body).to.eql({
 						message: 'User not found'
 					});
